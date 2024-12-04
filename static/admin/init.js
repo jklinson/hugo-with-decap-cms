c0c0140cf3a813d84d22a166d02acf7a69bf895d
Linson Alfred <jklinson@gmail.com>
2024-12-05 00:46:28 +1000

    github backend configuration update

--- static/admin/init.js
+++ static/admin/init.js
@@ -14,8 +14,8 @@ CMS.init({
             client_id: 'Ov23liUavsHCBFVwjSWu',
             client_secret: '1e5c506ec092db32290bbfae4d442f560eb72eb7',
             auth_endpoint: 'https://github.com/login/oauth/authorize',
-            access_token_url: 'https://github.com/login/oauth/access_token'
-
+            access_token_url: 'https://github.com/login/oauth/access_token',
+            proxy_url: null
         },
         load_config_file: false,
         media_folder: 'static/images/uploads',
